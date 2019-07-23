e821eb268c642b64dc6a5ab8d2f07e650ace3516
Stephen Soltesz <soltesz@google.com>
2019-07-23 10:39:40 -0400

    Add hourly binned test counts

---
+++ views/library/referenced-by/entry07_platform_hourly_downloads.sql
@@ -0,0 +1,39 @@
+#standardSQL
+
+WITH web100_lga03 AS (
+
+  SELECT
+    TIMESTAMP_TRUNC(start_time, HOUR) as hour, hostname, COUNT(*) as count
+  FROM `{{.ProjectID}}.library.entry07_web100_downloads`
+  WHERE
+      hostname LIKE '%lga03%'
+  AND mbps is not NULL
+  AND mbps > 0
+  AND duration > 9
+
+  GROUP BY hostname, hour
+  ORDER BY hostname, hour
+
+), result_lga03 AS (
+
+  SELECT
+    TIMESTAMP_TRUNC(start_time, hour) as hour,  hostname, count(*) as count
+  FROM `{{.ProjectID}}.library.entry07_result_downloads`
+  WHERE
+        hostname LIKE '%lga03%'
+
+    -- NOTE: without filtering, we find similar number of raw rows in BQ for both platforms.
+    -- When filtering mbps here, we find fewer total tests from the new platform.
+    -- Possibly b/c the web100 ndt server still saves parsable data while ndt-server
+    -- does not, or due to the ndt-server failing for some legacy clients.
+    AND mbps is not NULL
+    AND mbps > 0
+    AND duration > 9
+
+  GROUP BY hour, hostname
+  ORDER BY hostname, hour
+)
+
+SELECT * FROM web100_lga03
+UNION ALL
+SELECT * FROM result_lga03
