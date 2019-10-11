e2451cdd8fec968b7ffe8c32c3f4a281597f7526
Peter Boothe <pboothe@google.com>
2019-10-11 17:36:51 -0400

    Adds tcpinfo tables to the exported views

---
+++ views/ndt/tcpinfo.sql
@@ -0,0 +1,6 @@
+#standardSQL
+-- This is the root view that all other views in the ndt dataset are derived from.  The tcp-info
+-- data ought to be partitioned on a per-experiment basis. Right now this links to a too-large table
+-- of tcpinfo data that contains a superset of what it should.
+SELECT CAST(_PARTITIONTIME AS DATE) AS partition_date, *
+FROM `{{.ProjectID}}.base_tables.tcpinfo`
