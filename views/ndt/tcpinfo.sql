d8888346d90d668ad0d59ae0c77286df1a8ec801
Peter Boothe <pboothe@google.com>
2019-10-11 17:58:21 -0400

    Add todo

--- views/ndt/tcpinfo.sql
+++ views/ndt/tcpinfo.sql
@@ -1,6 +1,8 @@
 #standardSQL
--- This is the root view that all other views in the ndt dataset are derived from.  The tcp-info
--- data ought to be partitioned on a per-experiment basis. Right now this links to a too-large table
--- of tcpinfo data that contains a superset of what it should.
+-- This is the root view that all other views in the ndt dataset are derived from.
+--
+-- TODO(https://github.com/m-lab/etl-schema/issues/49)
+-- The tcp-info data ought to be partitioned on a per-experiment basis. Right now this links to a
+-- too-large table of tcpinfo data that contains a superset of what it should.
 SELECT CAST(_PARTITIONTIME AS DATE) AS partition_date, *
 FROM `{{.ProjectID}}.base_tables.tcpinfo`
