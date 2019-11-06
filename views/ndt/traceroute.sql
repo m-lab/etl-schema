1e2742906100a8bfe41be9bc96a76982a4603ffb
yachang <yachang@users.noreply.github.com>
2019-11-06 15:14:55 -0500

    Update traceroute.sql

--- views/ndt/traceroute.sql
+++ views/ndt/traceroute.sql
@@ -1,4 +1,8 @@
 #standardSQL
 -- This is the root view that all other views in the ndt dataset are derived from.
+--
+-- TODO(https://github.com/m-lab/etl-schema/issues/49)
+-- The traceroute data ought to be partitioned on a per-experiment basis. Right now this links to a
+-- too-large table of traceroute data that contains a superset of what it should.
 SELECT CAST(_PARTITIONTIME AS DATE) AS partition_date, *
 FROM `{{.ProjectID}}.base_tables.traceroute`
