0753de0f2f0e42c1c83507f83b8a8b0da8c30459
Stephen Soltesz <stephen.soltesz@gmail.com>
2020-03-27 18:21:59 -0400

    Remove test template

--- views/library/soltesz_reference_oti.sql
+++
@@ -1,9 +0,0 @@
-#StandardSQL
-
-SELECT "ndt5" as t, COUNT(*) FROM `mlab-oti.ndt.ndt5`
-UNION ALL
-SELECT "tcpinfo" as t, COUNT(*) FROM `mlab-oti.base_tables.tcpinfo`
-UNION ALL
-SELECT "traceroute" as t, COUNT(*) FROM `mlab-oti.batch.traceroute`
-UNION ALL
-SELECT "web100" as t, COUNT(*) FROM `mlab-oti.ndt.web100`
