82db81a66184afe54f411597afbbdeee7f53a3ca
Matt Mathis <mattmathis@google.com>
2020-10-23 17:25:27 -0700

    Describe old views as obsolete

--- views/ndt/ndt5.sql
+++ views/ndt/ndt5.sql
@@ -1,4 +1,7 @@
 #standardSQL
--- This is the ndt5 root view that all other tcpinfo-platform views in the ndt dataset are derived from.
+-- This was the root view under the old parser.
+-- It is now obsolete, and will be deprecated once the new parser is fully in production.
+--
+
 SELECT CAST(_PARTITIONTIME AS DATE) AS partition_date, *
 FROM `{{.ProjectID}}.base_tables.ndt5`
