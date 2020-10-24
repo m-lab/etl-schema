23c70815e71c141d0a764181885e8b473f7f1606
Matt Mathis <mattmathis@google.com>
2020-10-23 17:41:42 -0700

    Describe old views as obsolete

--- views/ndt/web100.sql
+++ views/ndt/web100.sql
@@ -1,4 +1,6 @@
 #standardSQL
--- This is the ndt root view that all other views in the ndt dataset are derived from.
+-- This was the root view under the old parser.
+-- It is now obsolete, and will be deprecated once the new parser is fully in production.
+--
 SELECT CAST(_PARTITIONTIME AS DATE) AS partition_date, *
 FROM `{{.ProjectID}}.base_tables.ndt`
