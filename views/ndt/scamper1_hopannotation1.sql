d4cad2b98cf8f9f8df267fb02f94d16b60d031da
Cristina Leon <cristinaleon@google.com>
2021-11-12 20:27:27 +0000

    Adding stats and querying previous week

--- views/ndt/scamper1_hopannotation1.sql
+++ views/ndt/scamper1_hopannotation1.sql
@@ -15,28 +15,33 @@
 -- Researchers are strongly encouraged to use this view
 -- to support their research and provide feedback.
 --
+-- The stats for querying this view are roughly the following:
+-- | Bytes processed | Slot time consumed | Bytes shuffled | Bytes spilled to disk | Number of rows |
+-- |-----------------|--------------------|----------------|-----------------------|----------------|
+-- | 687.5 GB        | 4 days 1 hr        | 10.13 TB       | 2.14 TB               | 109,259,807    |
+--
 
 WITH scamper1 AS (
     SELECT * FROM `{{.ProjectID}}.ndt.scamper1`
-    WHERE date BETWEEN "2021-11-01" AND "2021-11-07"
+    WHERE date BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 10 DAY) AND DATE_SUB(CURRENT_DATE(), INTERVAL 3 DAY)
 ),
 
 hops AS (
     SELECT scamper1.id, fh AS hop
     FROM scamper1 CROSS JOIN UNNEST(scamper1.raw.Tracelb.nodes) as fh
-	WHERE date BETWEEN "2021-11-01" AND "2021-11-07"
+	WHERE date BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 10 DAY) AND DATE_SUB(CURRENT_DATE(), INTERVAL 3 DAY)
 ),
 
 -- Annotate.
 annotated AS (
     SELECT hops.id,
-	STRUCT(hops.Hop.hop_id, hops.Hop.addr, hops.Hop.name, hops.Hop.q_ttl, hops.Hop.linkc, hops.Hop.links, ann.raw.Annotations AS annotations) as hop
+	STRUCT(hops.Hop.hop_id, hops.Hop.addr, hops.Hop.name, hops.Hop.q_ttl, hops.Hop.linkc, hops.Hop.links, ann.raw.Annotations AS exp_annotations) as exp_hop
     FROM hops LEFT JOIN `{{.ProjectID}}.raw_ndt.hopannotation1` as ann ON (hops.hop.hop_id = ann.id)
 ),
 
 -- Now reassemble the Hop arrays.
 mash AS (
-    SELECT id, ARRAY_AGG(hop) as hop
+    SELECT id, ARRAY_AGG(exp_hop) as exp_hop
     FROM annotated
     GROUP BY id
 )
@@ -44,7 +49,7 @@ mash AS (
 -- Recombine the hop arrays with top level fields.
 SELECT scamper1.* REPLACE (
     (SELECT AS STRUCT scamper1.raw.* REPLACE (
-        (SELECT AS STRUCT raw.Tracelb.* EXCEPT (nodes), mash.hop AS nodes
+        (SELECT AS STRUCT raw.Tracelb.* EXCEPT (nodes), mash.exp_hop AS exp_nodes
         ) AS Tracelb)
     ) AS raw
 )
