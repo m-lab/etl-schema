f388ebdb7508ba798628fecc16b134c5764c8159
critzo <critzo@measurementlab.net>
2022-01-20 11:42:48 -0500

    update statistics views to include all available years (#111)
    * update statistics views to include all available years
    
    * corrected one query

--- views/statistics/v0_global_asn.sql
+++ views/statistics/v0_global_asn.sql
@@ -1,13 +1,1 @@
-
-WITH
-global_asn_2020 AS (
-  SELECT * FROM `mlab-oti.statistics.global_asn_2020`
-),
-global_asn_2021 AS (
-  SELECT * FROM `mlab-oti.statistics.global_asn_2021`
-),
-all_years AS (
-  SELECT * FROM global_asn_2020
-  UNION ALL (SELECT * FROM global_asn_2021)
-)
-SELECT * FROM all_years
+SELECT * FROM `mlab-oti.statistics.global_asn_*`
