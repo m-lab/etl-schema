7d09c227886ff7819c7a49292413f5ba7454fb78
Chris Ritzo <critzo@measurementlab.net>
2022-01-20 10:43:10 -0500

    update statistics views to include all available years

--- views/statistics/v0_us_counties.sql
+++ views/statistics/v0_us_counties.sql
@@ -1,13 +1,1 @@
-
-WITH
-us_counties_2020 AS (
-  SELECT * FROM `mlab-oti.statistics.us_counties_2020`
-),
-us_counties_2021 AS (
-  SELECT * FROM `mlab-oti.statistics.us_counties_2021`
-),
-all_years AS (
-  SELECT * FROM us_counties_2020
-  UNION ALL (SELECT * FROM us_counties_2021)
-)
-SELECT * FROM all_years
+SELECT * FROM `mlab-oti.statistics.us_counties_*`
--- views/statistics/v0_us_states_asn.sql
+++ views/statistics/v0_us_states_asn.sql
@@ -1,13 +1,1 @@
-
-WITH
-us_states_asn_2020 AS (
-  SELECT * FROM `mlab-oti.statistics.us_states_asn_2020`
-),
-us_states_asn_2021 AS (
-  SELECT * FROM `mlab-oti.statistics.us_states_asn_2021`
-),
-all_years AS (
-  SELECT * FROM us_states_asn_2020
-  UNION ALL (SELECT * FROM us_states_asn_2021)
-)
-SELECT * FROM all_years
+SELECT * FROM all_years FROM `mlab-oti.statistics.us_states_asn_*`
