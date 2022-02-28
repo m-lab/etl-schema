9c2a980c3a25ffaed2ae88cb2dfaf84f28c84788
Cristina Leon <cristinaleon@google.com>
2022-02-28 15:41:52 +0000

    Add 'legacy' view for paris-traceroute

---
+++ views/ndt_raw/paris_legacy.sql
@@ -0,0 +1,6 @@
+@@ -0,0 +1,4 @@
+#standardSQL
+-- This was the root paris-traceroute view under the old parser.
+-- It will be deprecated once the new parser is in production.
+SELECT CAST(_PARTITIONTIME AS DATE) AS partition_date, *
+FROM `{{.ProjectID}}.base_tables.traceroute`
