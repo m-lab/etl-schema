7bbc26f65b8b911906aace23de42ce447fb25a6d
Stephen Soltesz <stephen.soltesz@gmail.com>
2022-03-10 12:45:55 -0500

    Update ndt5 extended views (#112)
    * Update ndt5 extended views
    * Include date in join
    * Change internal column date suffix

--- views/ndt_intermediate/extended_ndt5_downloads.sql
+++ views/ndt_intermediate/extended_ndt5_downloads.sql
@@ -9,14 +9,14 @@
 --
 
 WITH ndt5downloads AS (
-  SELECT partition_date, ParseInfo, result.S2C,
-  (result.S2C.Error != "") AS IsErrored,
-  TIMESTAMP_DIFF(result.S2C.EndTime, result.S2C.StartTime, MICROSECOND) AS connection_duration
-  FROM   `{{.ProjectID}}.ndt_raw.ndt5_legacy` -- TODO move to intermediate_ndt
+  SELECT date, parser, raw.S2C, client, server,
+  (raw.S2C.Error != "") AS IsErrored,
+  TIMESTAMP_DIFF(raw.S2C.EndTime, raw.S2C.StartTime, MICROSECOND) AS connection_duration
+  FROM   `{{.ProjectID}}.ndt.ndt5` -- TODO move to intermediate_ndt
   -- Limit to valid S2C results
-  WHERE result.S2C IS NOT NULL
-  AND result.S2C.UUID IS NOT NULL
-  AND result.S2C.UUID NOT IN ( '', 'ERROR_DISCOVERING_UUID' )
+  WHERE raw.S2C IS NOT NULL
+  AND raw.S2C.UUID IS NOT NULL
+  AND raw.S2C.UUID NOT IN ( '', 'ERROR_DISCOVERING_UUID' )
 ),
 
 tcpinfo AS (
@@ -26,7 +26,7 @@ tcpinfo AS (
 
 PreCleanNDT5 AS (
   SELECT
-    downloads.*, tcpinfo.Client, tcpinfo.Server,
+    downloads.*,
     tcpinfo.FinalSnapshot AS FinalSnapshot,
     -- Any loss implys a netowork bottleneck
     (FinalSnapshot.TCPInfo.TotalRetrans > 0) AS IsCongested,
@@ -49,23 +49,23 @@ PreCleanNDT5 AS (
                 12) = NET.IP_FROM_STRING("172.16.0.0"))
       OR (NET.IP_TRUNC(NET.SAFE_IP_FROM_STRING(downloads.S2C.ServerIP),
                 16) = NET.IP_FROM_STRING("192.168.0.0"))
-      OR REGEXP_EXTRACT(downloads.ParseInfo.TaskFileName, '(mlab[1-4])-[a-z][a-z][a-z][0-9][0-9t]') = 'mlab4'
+      OR REGEXP_EXTRACT(downloads.parser.ArchiveURL, '(mlab[1-4])-[a-z][a-z][a-z][0-9][0-9t]') = 'mlab4'
     ) AS IsOAM,  -- Data is not from valid clients
     tcpinfo.ParseInfo AS TCPparser,
-    downloads.ParseInfo AS NDT5parser,
+    downloads.parser AS NDT5parser,
   FROM
     -- Use a left join to allow NDT test without matching tcpinfo rows.
     ndt5downloads AS downloads
     LEFT JOIN tcpinfo
     ON
-#     downloads.partition_date = tcpinfo.partition_date AND -- This may exclude a few rows issue:#63
+      downloads.date = tcpinfo.partition_date AND -- This may exclude a few rows issue:#63
       downloads.S2C.UUID = tcpinfo.UUID
 ),
 
 NDT5DownloadModels AS (
   SELECT
     S2C.UUID AS id,
-    partition_date as date,
+    date,
     STRUCT (
       -- NDT unified fields: Upload/Download/RTT/Loss/CCAlg + Geo + ASN
       S2C.UUID,
@@ -102,71 +102,18 @@ NDT5DownloadModels AS (
     STRUCT (
       S2C.ClientIP AS IP,
       S2C.ClientPort AS Port,
-      -- Legacy Geo approximates dev.maxmind.com/geoip/geoip2/geoip2-city-country-csv-databases/
-      STRUCT (
-             client.Geo.continent_code, -- aka ContinentCode
-             client.Geo.country_code, -- aka CountryCode
-             client.Geo.country_code3, -- aka CountryCode3
-             client.Geo.country_name, -- aka CountryName
-             client.Geo.region, -- aka Region
-             '' AS Subdivision1ISOCode, -- MISSING
-             '' AS Subdivision1Name, -- MISSING
-             '' AS Subdivision2ISOCode, -- MISSING
-             '' AS Subdivision2Name, -- MISSING
-             client.Geo.metro_code, -- aka MetroCode
-             client.Geo.city, -- aka City
-             client.Geo.area_code, -- aka AreaCode
-             client.Geo.postal_code, -- aka PostalCode
-             client.Geo.latitude, -- aka Latitude
-             client.Geo.longitude, -- aka Longitude
-             client.Geo.radius, -- aka AccuracyRadiusKm
-             FALSE AS Missing -- Future missing record flag
-      ) AS Geo,
-#      Client.Network -- BUG still old schema
-      STRUCT (
-        client.Network.IPPrefix AS CIDR,
-        client.Network.Systems[SAFE_OFFSET(0)].ASNs[SAFE_OFFSET(0)] AS ASNumber,
-        '' AS ASName, -- MISSING
-        False AS Missing, -- MISSING
-        client.Network.Systems -- Includes ASNs, etc
-      ) AS Network
+      client.Geo,
+      client.Network
     ) AS client,
     STRUCT (
       S2C.ServerIP AS IP,
       S2C.ServerPort AS Port,
-      REGEXP_EXTRACT(ParseInfo.TaskFileName,
-            'mlab[1-4]-([a-z][a-z][a-z][0-9][0-9t])') AS Site, -- e.g. lga02
-      REGEXP_EXTRACT(ParseInfo.TaskFileName,
-            '(mlab[1-4])-[a-z][a-z][a-z][0-9][0-9t]') AS Machine, -- e.g. mlab1
-      STRUCT (
-             server.Geo.continent_code, -- aka ContinentCode
-             server.Geo.country_code, -- aka CountryCode
-             server.Geo.country_code3, -- aka CountryCode3
-             server.Geo.country_name, -- aka CountryName
-             server.Geo.region, -- aka Region
-             '' AS Subdivision1ISOCode, -- MISSING
-             '' AS Subdivision1Name, -- MISSING
-             '' AS Subdivision2ISOCode, -- MISSING
-             '' AS Subdivision2Name, -- MISSING
-             server.Geo.metro_code, -- aka MetroCode
-             server.Geo.city, -- aka City
-             server.Geo.area_code, -- aka AreaCode
-             server.Geo.postal_code, -- aka PostalCode
-             server.Geo.latitude, -- aka Latitude
-             server.Geo.longitude, -- aka Longitude
-             server.Geo.radius, -- aka AccuracyRadiusKm
-             FALSE AS Missing -- Future missing record flag
-      ) AS Geo,
-#     Server.Network -- BUG still old schema
-      STRUCT (
-        server.Network.IPPrefix AS CIDR,
-        server.Network.Systems[SAFE_OFFSET(0)].ASNs[SAFE_OFFSET(0)] AS ASNumber,
-        '' AS ASName, -- MISSING
-        False AS Missing, -- MISSING
-        server.Network.Systems -- Includes ASNs, etc
-      ) AS Network
+      server.Site,
+      server.Machine,
+      server.Geo,
+      server.Network
     ) AS server,
-    PreCleanNDT5 AS _internal202010  -- Not stable and subject to breaking changes
+    PreCleanNDT5 AS _internal202201  -- Not stable and subject to breaking changes
   FROM PreCleanNDT5
 )
 
