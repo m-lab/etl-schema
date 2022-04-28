1f7797a6cc0523024496fe8cf9ee6d82016b25b1
Stephen Soltesz <stephen.soltesz@gmail.com>
2022-04-28 13:21:41 -0400

    Update pass-through query

--- views/ndt/web100.sql
+++ views/ndt/web100.sql
@@ -1,9 +1,9 @@
 --
--- This view is a pass-through for date partitioned ndt web100 data. This is
--- synthetically derived from the v1 data pipeline output for the ndt web100
--- dataset. It is "static" because it is not actively reprocessed. While it
--- uses standard column conventions, the schema is not guaranteed to be
--- backward compatible b/c there is currently no parser that supports
--- reprocessing this format.
+-- This view is a pass-through for date partitioned ndt web100 data. The data
+-- in this table is a static transformation of data from the v1 data pipeline
+-- for the ndt web100 dataset. It is "static" because it is not actively
+-- reprocessed. While it uses standard column conventions, the schema is not
+-- guaranteed to be backward compatible b/c there is currently no parser that
+-- supports reprocessing this format.
 --
-SELECT * FROM `{{.ProjectID}}.ndt.web100_static`
+SELECT * FROM `{{.ProjectID}}.ndt.web100`
