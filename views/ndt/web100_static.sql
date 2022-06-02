d7b148962ed961502a813fe17fd8de943bea0e8e
Matt Mathis <mattmathis@google.com>
2022-06-01 17:39:11 -0700

    Add pasthrough views for web100_static

---
+++ views/ndt/web100_static.sql
@@ -0,0 +1,6 @@
+--
+-- This view is a pass-through for web100_static data, which was
+-- parsed by the p1 parser, and manually converted to the v2 format.
+--
+--
+SELECT * FROM `{{.ProjectID}}.ndt.web100_static`
