a178f98a9eb5ac4ba2810eef1805842d4dcdee6d
Matt Mathis <mattmathis@google.com>
2022-06-01 17:51:09 -0700

    Revert "Add pasthrough views for web100_static"
    This reverts commit d7b148962ed961502a813fe17fd8de943bea0e8e.

--- views/ndt/web100_static.sql
+++
@@ -1,6 +0,0 @@
---
--- This view is a pass-through for web100_static data, which was
--- parsed by the p1 parser, and manually converted to the v2 format.
---
---
-SELECT * FROM `{{.ProjectID}}.ndt.web100_static`
