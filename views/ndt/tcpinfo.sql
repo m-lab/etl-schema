840fb893c373dbbae22c45b0f2f8868c35976652
Stephen Soltesz <stephen.soltesz@gmail.com>
2023-03-30 17:58:32 -0400

    Update comments

--- views/ndt/tcpinfo.sql
+++ views/ndt/tcpinfo.sql
@@ -1,5 +1,5 @@
 --
 -- This view is a pass-through for annotated tcpinfo data.  This materializes data
--- from ndt_raw.tcpinfo with ndt_raw.annotation into a single location.
+-- from ndt_raw.tcpinfo with ndt_raw.annotation2 into a single location.
 --
 SELECT * FROM `{{.ProjectID}}.ndt.tcpinfo`
