768850494660321f430ef0e24ef7c8448f22e190
Stephen Soltesz <stephen.soltesz@gmail.com>
2023-04-06 15:34:18 -0400

    Add pass-through views for wehe

---
+++ views/wehe_raw/annotation2.sql
@@ -0,0 +1,3 @@
+-- This is the raw wehe annotation2 view.
+--
+SELECT * FROM `{{.ProjectID}}.raw_wehe.annotation2`
---
+++ views/wehe_raw/hopannotation2.sql
@@ -0,0 +1,3 @@
+-- This is the raw hopannotation2 view.
+--
+SELECT * FROM `{{.ProjectID}}.raw_wehe.hopannotation2`
