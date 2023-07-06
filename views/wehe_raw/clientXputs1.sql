6a4e6376db5defb047ed2a9e89f531d36b16fb9e
Stephen Soltesz <stephen.soltesz@gmail.com>
2023-07-06 17:31:25 -0400

    Add wehe autoloaded passthrough views

---
+++ views/wehe_raw/clientXputs1.sql
@@ -0,0 +1,3 @@
+-- This is the raw wehe clientXputs1 view.
+--
+SELECT * FROM `{{.ProjectID}}.raw_wehe.clientXputs1`
---
+++ views/wehe_raw/replayInfo1.sql
@@ -0,0 +1,3 @@
+-- This is the raw wehe replayInfo1 view.
+--
+SELECT * FROM `{{.ProjectID}}.raw_wehe.replayInfo1`
