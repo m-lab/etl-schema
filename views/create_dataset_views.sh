54ad5a13ea603b5573bb36bcf4bd5973bc570d92
Cristina Leon <cristinaleon@google.com>
2023-09-26 20:00:02 +0000

    Add _raw suffix to revtr autoloaded tables

--- views/create_dataset_views.sh
+++ views/create_dataset_views.sh
@@ -120,10 +120,10 @@ if [[ ${DST_PROJECT} = "measurement-lab" ]] ; then
     create_view ${SRC_PROJECT} ${DST_PROJECT} wehe ./wehe/scamper1_hopannotation2.sql
 
     # REVTR
-   create_view ${SRC_PROJECT} ${DST_PROJECT} revtr ./revtr/ping1.sql
-   create_view ${SRC_PROJECT} ${DST_PROJECT} revtr ./revtr/revtr1.sql
-   create_view ${SRC_PROJECT} ${DST_PROJECT} revtr ./revtr/trace1.sql
-   create_view ${SRC_PROJECT} ${DST_PROJECT} revtr ./revtr/traceatlas1.sql
+   create_view ${SRC_PROJECT} ${DST_PROJECT} revtr_raw ./revtr_raw/ping1.sql
+   create_view ${SRC_PROJECT} ${DST_PROJECT} revtr_raw ./revtr_raw/revtr1.sql
+   create_view ${SRC_PROJECT} ${DST_PROJECT} revtr_raw ./revtr_raw/trace1.sql
+   create_view ${SRC_PROJECT} ${DST_PROJECT} revtr_raw ./revtr_raw/traceatlas1.sql
 fi
 
 # NDT extended (mixed parsers)
