1fcff04f13f554fa48eb6a3cdf5d3215f4619781
zeinabshmeis <zeinab.shmeis@epfl.ch>
2023-11-06 12:16:49 +0100

    fix queries to do join with info

--- views/wehe/clientXputs1.sql
+++ views/wehe/clientXputs1.sql
@@ -3,5 +3,8 @@
 -- that are not CLI (userID does not start with @)
 -- and carried over mobile network
 --
-SELECT * FROM `{{.ProjectID}}.raw_wehe.clientXputs1`
-WHERE NOT (raw.userID LIKE '@%') AND (raw.metadata.updatedCarrierName like '%(cellular)')
+SELECT xput.* 
+FROM `{{.ProjectID}}.wehe_raw.clientXputs1` as xput
+INNER JOIN `{{.ProjectID}}.wehe_raw.replayInfo1` as info
+ON xput.raw.userID = info.raw.userID AND CAST(xput.raw.historyCount AS INT64) = CAST(info.raw.historyCount AS INT64) AND xput.raw.testID = info.raw.testID
+WHERE NOT (info.raw.userID LIKE '@%') AND (info.raw.metadata.updatedCarrierName like '%(cellular)')
--- views/wehe/decisions1.sql
+++ views/wehe/decisions1.sql
@@ -2,6 +2,9 @@
 -- This view shows decisions1 from raw wehe for users
 -- that are not CLI (userID does not start with @)
 -- and carried over mobile network
---s
-SELECT * FROM `{{.ProjectID}}.raw_wehe.decisions1`
-WHERE NOT (raw.userID LIKE '@%') AND (raw.metadata.updatedCarrierName like '%(cellular)')
+--
+SELECT result.* 
+FROM `{{.ProjectID}}.raw_wehe.decisions1` as result
+INNER JOIN `{{.ProjectID}}.wehe_raw.replayInfo1` as info
+ON result.raw.userID = info.raw.userID AND CAST(result.raw.historyCount AS INT64) = CAST(info.raw.historyCount AS INT64) AND result.raw.testID = info.raw.testID
+WHERE NOT (info.raw.userID LIKE '@%') AND (info.raw.metadata.updatedCarrierName like '%(cellular)')
