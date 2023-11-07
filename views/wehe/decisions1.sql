ff6699d9dc7636ea443e48c2782243619c332ae7
zeinabshmeis <zeinab.shmeis@epfl.ch>
2023-11-07 09:54:14 +0100

    add date filter

--- views/wehe/decisions1.sql
+++ views/wehe/decisions1.sql
@@ -6,5 +6,8 @@
 SELECT result.* 
 FROM `{{.ProjectID}}.raw_wehe.decisions1` as result
 INNER JOIN `{{.ProjectID}}.wehe_raw.replayInfo1` as info
-ON result.raw.userID = info.raw.userID AND CAST(result.raw.historyCount AS INT64) = CAST(info.raw.historyCount AS INT64) AND result.raw.testID = info.raw.testID
+ON result.raw.userID = info.raw.userID 
+   AND CAST(result.raw.historyCount AS INT64) = CAST(info.raw.historyCount AS INT64) 
+   AND result.raw.testID = info.raw.testID 
+   AND result.date = info.date
 WHERE NOT (info.raw.userID LIKE '@%') AND (info.raw.metadata.updatedCarrierName like '%(cellular)')
