aaf08a6d2135a9dbb38313dc34c261e8546b6db4
Stephen Soltesz <stephen.soltesz@gmail.com>
2024-09-18 16:59:49 -0400

    Separate script from library functions

--- views/create_dataset_views.sh
+++ views/create_dataset_views.sh
@@ -19,58 +19,13 @@ KEYNAME=${1:?Please provide a key name to authorize operations or "self"}
 SRC_PROJECT=${2:?Please provide source project: $USAGE}
 DST_PROJECT=${3:?Please provide destination project: $USAGE}
 
+# Setup environment.
 BASEDIR=$( realpath $( dirname "${BASH_SOURCE[0]}" ) )
 cd ${BASEDIR}
 
-# Git info is nominally exported from the caller
-if [ -z "${TAG_NAME-}" -o -z "${COMMIT_SHA-}" ]; then
-  echo "Not Git"
-  export TAG_NAME="manual"
-  export COMMIT_SHA="undefined"
-fi
-
-if [[ "${KEYNAME}" != "self" ]] ; then
-  echo "${!KEYNAME}" > /tmp/sa.json
-  export GOOGLE_APPLICATION_CREDENTIALS=/tmp/sa.json
-  # Guarantee that `gcloud config get-value accounnt` works as intended.
-  gcloud auth activate-service-account --key-file /tmp/sa.json
-fi
-# Extract service account user name.
-USER=$( gcloud config get-value account )
-
-BQ_CREATE_VIEW=bq_create_view
-if [[ -x ${BASEDIR}/bq_create_view ]] ; then
-  BQ_CREATE_VIEW=${BASEDIR}/bq_create_view
-fi
-
-function create_view() {
-  local src_project=$1
-  local dst_project=$2
-  local dataset=$3
-  local template=$4
-
-  description=$(
-    awk '/^--/ {print substr($0, 3)} /^SELECT/ {exit(0)}' ${template} )
-  description+=$'\n'$'\n'"Release tag: $TAG_NAME Commit: $COMMIT_SHA"
-  description+=$'\n'"View of data from '${src_project}'."
-  description+=$'\n'"Using: github.com/m-lab/..${template}"
-  description+=$'\n'"On :"`date`
-
-  # Strip filename down to view name.
-  # Note that _nofilter views are generated with .SQL~ suffix to prevent checkin
-  view="${template%%.sql}"
-  view="${view%%.SQL~}"
-  view="${view##*/}"
-
-  echo "Creating "${dst_project}.${dataset}.${view}" using "${template}
-
-  ${BQ_CREATE_VIEW} \
-      -src-project "${src_project}" \
-      -create-view "${dst_project}.${dataset}.${view}" \
-      -template "${template}" \
-      -description "${description}" \
-      -editor "${USER}"
-}
+# Initialize library.
+source ${BASEDIR}/create_view_lib.sh
+create_view_init
 
 # Build all views
 # Upper level views always have src_project=dst_project=DST_PROJECT
