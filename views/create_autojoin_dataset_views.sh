23ea61ebc3d403cbc2486c1ebb534a46bf02b4d1
Stephen Soltesz <stephen.soltesz@gmail.com>
2024-09-19 17:16:26 -0400

    Generate queries dynamically

--- views/create_autojoin_dataset_views.sh
+++ views/create_autojoin_dataset_views.sh
@@ -24,8 +24,19 @@ source ${BASEDIR}/create_view_lib.sh
 create_view_init
 
 echo "Creating autojoin views"
-create_org_joined_view  ${SRC_PROJECT} mlab
-create_org_joined_view ${SRC_PROJECT} rnp
+# TODO(soltesz): this should all be automated.
+# Get list of orgs with autoloaded data.
+datasets=$( bq ls --project_id ${SRC_PROJECT} | grep autoload | grep _ndt | grep -v autoload_v2_ndt )
+echo '-- Generated query' > ./autoload_v2_ndt/ndt7_union.sql
+for ds in $datasets ; do
+  org=$( echo $ds | tr '_' ' ' | awk '{print $3}' )
+  create_org_joined_view  ${SRC_PROJECT} ${org}
+  if grep -q SELECT ./autoload_v2_ndt/ndt7_union.sql ; then
+    echo 'UNION ALL' >> ./autoload_v2_ndt/ndt7_union.sql
+  fi
+  echo 'SELECT * FROM `{{.ProjectID}}.'$ds'.ndt7_joined`' >> ./autoload_v2_ndt/ndt7_union.sql
+done
+
 # NOTE: Must create "ndt7_union" last because it references the views above.
 create_view ${SRC_PROJECT} ${SRC_PROJECT} autoload_v2_ndt ./autoload_v2_ndt/ndt7_union.sql
 
