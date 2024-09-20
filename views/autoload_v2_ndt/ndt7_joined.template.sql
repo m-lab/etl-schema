b66d8dfbc96e6cb7d59d8347dfee299d42dd1206
Stephen Soltesz <stephen.soltesz@gmail.com>
2024-09-20 10:49:32 -0400

    Simplify query logic for readability

--- views/autoload_v2_ndt/ndt7_joined.template.sql
+++ views/autoload_v2_ndt/ndt7_joined.template.sql
@@ -1,39 +1,35 @@
-WITH ndt7 AS (
+WITH prendt7 AS (
+  SELECT
+    raw.Download IS NOT NULL AS isDownload,
+    raw.Upload IS NOT NULL AS isUpload,
+    ARRAY_REVERSE(raw.Download.ServerMeasurements)[SAFE_OFFSET(0)].BBRInfo IS NOT NULL AS isBBR,
+    ARRAY_REVERSE(raw.Download.ServerMeasurements)[SAFE_OFFSET(0)].TCPInfo.BytesAcked AS downloadBytesAcked,
+    ARRAY_REVERSE(raw.Download.ServerMeasurements)[SAFE_OFFSET(0)].TCPInfo.ElapsedTime AS downloadElapsedTime,
+    ARRAY_REVERSE(raw.Download.ServerMeasurements)[SAFE_OFFSET(0)].TCPInfo.MinRTT AS downloadMinRTT,
+    ARRAY_REVERSE(raw.Download.ServerMeasurements)[SAFE_OFFSET(0)].TCPInfo.BytesRetrans AS downloadBytesRetrans,
+    ARRAY_REVERSE(raw.Download.ServerMeasurements)[SAFE_OFFSET(0)].TCPInfo.BytesSent AS downloadBytesSent,
+    ARRAY_REVERSE(raw.Upload.ServerMeasurements)[SAFE_OFFSET(0)].TCPInfo.BytesReceived AS uploadBytesReceived,
+    ARRAY_REVERSE(raw.Upload.ServerMeasurements)[SAFE_OFFSET(0)].TCPInfo.ElapsedTime AS uploadElapsedTime,
+    ARRAY_REVERSE(raw.Upload.ServerMeasurements)[SAFE_OFFSET(0)].TCPInfo.MinRTT AS uploadMinRTT,
+    *
+  FROM `{{.ProjectID}}.autoload_v2_{{ORG}}_ndt.ndt7_raw`
+
+), ndt7 AS (
   SELECT
     -- Pick the download or upload UUID per row.
-    IF(raw.Download IS NOT NULL,
-       raw.Download.UUID,
-       IF(raw.Upload IS NOT NULL,
-          raw.Upload.UUID, NULL)) AS id,
+    IF(isDownload, raw.Download.UUID, IF(isUpload, raw.Upload.UUID, NULL)) AS id,
     -- Construct the summary 'a' record for compatibility with standard columns.
     STRUCT (
-      IF(raw.Download IS NOT NULL,
-         raw.Download.UUID,
-         IF(raw.Upload IS NOT NULL, raw.Upload.UUID, NULL)) AS UUID,
-      IF(raw.Download IS NOT NULL,
-         raw.Download.StartTime,
-         IF(raw.Upload IS NOT NULL, raw.Upload.StartTime, NULL)) AS TestTime,
-      -- TODO(soltesz): read this from the snapshots instead. Some BYOS nodes may be misconfigured.
-      "bbr" AS CongestionControl,
-      8 * IF(raw.Download IS NOT NULL,
-             ARRAY_REVERSE(raw.Download.ServerMeasurements)[SAFE_OFFSET(0)].TCPInfo.BytesAcked /
-               ARRAY_REVERSE(raw.Download.ServerMeasurements)[SAFE_OFFSET(0)].TCPInfo.ElapsedTime,
-             IF(raw.Upload IS NOT NULL,
-                ARRAY_REVERSE(raw.Upload.ServerMeasurements)[SAFE_OFFSET(0)].TCPInfo.BytesAcked /
-                  ARRAY_REVERSE(raw.Upload.ServerMeasurements)[SAFE_OFFSET(0)].TCPInfo.ElapsedTime, NULL)) AS MeanThroughputMbps,
-      IF(raw.Download IS NOT NULL,
-         ARRAY_REVERSE(raw.Download.ServerMeasurements)[SAFE_OFFSET(0)].TCPInfo.MinRTT,
-         IF(raw.Upload IS NOT NULL,
-            ARRAY_REVERSE(raw.Upload.ServerMeasurements)[SAFE_OFFSET(0)].TCPInfo.MinRTT, NULL)) / 1000 AS MinRTT, -- unit: ms
-      IF(raw.Download IS NOT NULL,
-         ARRAY_REVERSE(raw.Download.ServerMeasurements)[SAFE_OFFSET(0)].TCPInfo.BytesRetrans /
-           ARRAY_REVERSE(raw.Download.ServerMeasurements)[SAFE_OFFSET(0)].TCPInfo.BytesSent,
-         IF(raw.Upload IS NOT NULL,
-            ARRAY_REVERSE(raw.Upload.ServerMeasurements)[SAFE_OFFSET(0)].TCPInfo.BytesRetrans /
-              ARRAY_REVERSE(raw.Upload.ServerMeasurements)[SAFE_OFFSET(0)].TCPInfo.BytesSent, NULL)) AS LossRate
+      IF(isDownload, raw.Download.UUID, IF(isUpload, raw.Upload.UUID, NULL)) AS UUID,
+      IF(isDownload, raw.Download.StartTime, IF(isUpload, raw.Upload.StartTime, NULL)) AS TestTime,
+      IF(isBBR, "bbr", "unknown") AS CongestionControl,
+      8 * IF(isDownload, downloadBytesAcked / downloadElapsedTime,
+             IF(isUpload,  uploadBytesReceived / uploadElapsedTime, NULL)) AS MeanThroughputMbps,
+      IF(isDownload, downloadMinRTT, IF(isUpload, uploadMinRTT, NULL)) / 1000 AS MinRTT, -- unit: ms
+      IF(isDownload, downloadBytesRetrans / downloadBytesSent, IF(isUpload, 0, NULL)) AS LossRate
     ) AS a,
   *
-  FROM `{{.ProjectID}}.autoload_v2_{{ORG}}_ndt.ndt7_raw`
+  FROM prendt7
 ), ann2 AS (
   SELECT raw.UUID AS id, *
   FROM `{{.ProjectID}}.autoload_v2_{{ORG}}_ndt.annotation2_raw`
