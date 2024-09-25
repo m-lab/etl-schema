ad3e97b9535a47add3b7cbed1aac2be8932040d2
Stephen Soltesz <stephen.soltesz@gmail.com>
2024-09-24 21:12:40 -0400

    Remove other case

--- functions/ops.ndt7_download_pdf_autonode.sql
+++ functions/ops.ndt7_download_pdf_autonode.sql
@@ -36,22 +36,6 @@ AS (
      AND NOT TIMESTAMP_DIFF(raw.Download.EndTime, raw.Download.StartTime, MILLISECOND) > 60000 -- IsLong
      AND server.Site IS NOT NULL
 
-    UNION ALL
-    SELECT server.Site as site,
-      CASE field
-        WHEN "MeanThroughputMbps" THEN a.MeanThroughputMbps
-        WHEN "MinRTT" THEN a.MinRTT
-        WHEN "LossRate" THEN a.LossRate
-        ELSE 0
-      END AS metric
-    FROM `measurement-lab.ndt_intermediate.extended_ndt7_downloads`
-    WHERE date BETWEEN startDate AND endDate
-     AND REGEXP_CONTAINS(server.Site, siteRegex)
-     AND (filter.IsComplete AND filter.IsProduction AND NOT filter.IsError AND
-          NOT filter.IsOAM AND NOT filter.IsPlatformAnomaly AND NOT filter.IsSmall AND
-          (filter.IsEarlyExit OR NOT filter.IsShort) AND NOT filter.IsLong AND NOT filter._IsRFC1918)
-     AND server.Site IS NOT NULL
-
   ), ndt7_cross_xbins AS (
 
     SELECT
