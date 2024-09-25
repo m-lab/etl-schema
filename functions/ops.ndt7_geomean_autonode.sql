1dfdf805dbbb99deaacd81e0b20240fd0c5d0efb
Stephen Soltesz <stephen.soltesz@gmail.com>
2024-09-24 22:08:45 -0400

    Fix names and contents

--- functions/ops.ndt7_geomean_autonode.sql
+++ functions/ops.ndt7_geomean_autonode.sql
@@ -23,12 +23,17 @@ AS (
         WHEN "LossRate" THEN a.LossRate
         ELSE 0
       END))) AS geoMean,
-  FROM `measurement-lab.ndt_intermediate.extended_ndt7_downloads`
+  FROM `mlab-autojoin.autoload_v2_ndt.ndt7_union`
   WHERE date BETWEEN startDate AND endDate
-    AND REGEXP_CONTAINS(server.Site, siteRegex)
-    AND (filter.IsComplete AND filter.IsProduction AND NOT filter.IsError AND
-          NOT filter.IsOAM AND NOT filter.IsPlatformAnomaly AND NOT filter.IsSmall AND
-          (filter.IsEarlyExit OR NOT filter.IsShort) AND NOT filter.IsLong AND NOT filter._IsRFC1918)
-    AND server.Site IS NOT NULL
+     AND REGEXP_CONTAINS(server.Site, siteRegex)
+     AND raw.Download IS NOT NULL
+     AND ARRAY_LENGTH(raw.Download.ServerMeasurements) > 0 -- IsComplete
+     AND NOT (raw.Download.ServerMeasurements[SAFE_ORDINAL(ARRAY_LENGTH(raw.Download.ServerMeasurements))].TCPInfo.BytesAcked < 8192) -- IsSmall
+     AND (
+      IF("early_exit" IN (SELECT metadata.Name FROM UNNEST(raw.Download.ClientMetadata) AS metadata), True, False) OR
+      NOT TIMESTAMP_DIFF(raw.Download.EndTime, raw.Download.StartTime, MILLISECOND) < 9000 -- IsShort
+     )
+     AND NOT TIMESTAMP_DIFF(raw.Download.EndTime, raw.Download.StartTime, MILLISECOND) > 60000 -- IsLong
+     AND server.Site IS NOT NULL
   GROUP BY server.Site
 )
