8f79c4503f13ab567831781c226226b3405d40b3
Matt Mathis <matt.mathis@gmail.com>
2025-06-16 14:13:22 -0700

    use existing pass through to access mlab-autojoin project

--- views/ndt/ndt7_autojoin.sql
+++ views/ndt/ndt7_autojoin.sql
@@ -1,4 +1,4 @@
 --
 -- This view is a pass-through for ndt7 data from the autojoin fleet
 --
-SELECT * FROM `{{.ProjectID}}.mlab-autojoin.ndt7_union`
+SELECT * FROM `{{.ProjectID}}.autojoin_autoload_v2_ndt.ndt7_union`
--- views/ndt/ndt7_union.sql
+++ views/ndt/ndt7_union.sql
@@ -2,6 +2,6 @@
 -- This view is a union pass-through for annotated ndt7 data.
 -- It materializes data from the legacy and autojoin fleets in one location
 --
-SELECT * EXCEPT ( archiver ) FROM  `{{.ProjectID}}.mlab-autojoin.ndt7_union`
+SELECT * EXCEPT ( archiver ) FROM  `{{.ProjectID}}.autojoin_autoload_v2_ndt.ndt7_union`
   UNION ALL
 SELECT * EXCEPT ( Parser ) FROM `{{.ProjectID}}.ndt.ndt7`
